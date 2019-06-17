045a4faf5c415fe90d9ef6683e21002961442b86
srafi1 <shakilrafi0@gmail.com>
2019-06-16 22:48:15 -0400

    empty profile page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { AuthenticatedRoute } from './components/AuthenticatedRoute';
+import { isAuthenticated } from './utilities/auth';
 import Home from './components/Home';
 import Login from './components/Login';
+import Profile from './components/Profile';
 import './index.css';
 
-const router = (
-  <Router>
+let nav;
+if (isAuthenticated()) {
+  nav = (
+    <nav>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        <Link to="/profile">Profile</Link>
+      </div>
+    </nav>
+    );
+} else {
+  nav = (
     <nav>
       <div>
         <Link to="/">Home</Link>
@@ -17,9 +32,16 @@ const router = (
         <Link to="/login">Login / Register</Link>
       </div>
     </nav>
+    );
+}
+
+const router = (
+  <Router>
+    {nav}
 
     <Route exact path="/" component={Home} />
     <Route path="/login" component={Login} />
+    <AuthenticatedRoute path="/profile" component={Profile} />
   </Router>
 );
 
