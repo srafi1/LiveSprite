a30a1bbfa4f6313c2e6ea506dd1d3eddf77e439f
srafi1 <shakilrafi0@gmail.com>
2019-06-20 23:08:17 -0400

    fixed name on home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,7 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Live Pixel</h1>
+        <h1>Live Sprite</h1>
         <img src={mario} alt="mario" />
         <p>Animate pixel art</p>
         <Link to="/login" className="button">Get Started</Link>
