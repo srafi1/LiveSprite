4fd2ce30b1201bac240541f86004225497bb42ff
srafi1 <shakilrafi0@gmail.com>
2019-10-01 20:19:27 -0400

    started backend in expressjs

---
+++ express-backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
+
+app.listen(PORT, () => console.log(`LiveSprite API running on port ${PORT}`));
