221162500ac99bd3d43e637a7f87050138f71ba9
srafi1 <shakilrafi0@gmail.com>
2019-10-05 14:24:48 -0400

    register and login routes

--- express-backend/router.js
+++ express-backend/router.js
@@ -1,19 +1,77 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const cookieParser = require('cookie-parser');
 
 const User = require('./models/User');
 
+const upload = multer();
 const router = express.Router();
+
+router.use(bodyParser.json()); 
+router.use(bodyParser.urlencoded({ extended: true })); 
+router.use(upload.array());
+router.use(cookieParser());
 
 router.get('/', (req, res) => {
   res.send('LiveSprite API');
 });
 
 router.get('/register', (req, res) => {
-  res.send('registering user');
+  // get form inputs
+  let username = req.body['username'];
+  let password = req.body['password'];
+  let confirmPassword = req.body['confirmPassword'];
+
+  // form validation
+  if (!username || username.trim() === '') {
+    res.json({success: false, warning: 'Username is required'});
+  } else if (!password || password.trim() === '') {
+    res.json({success: false, warning: 'Password is required'});
+  } else if (!confirmPassword || password.trim() !== confirmPassword.trim()) {
+    res.json({success: false, warning: 'Passwords must match'});
+  } else {
+    // check if username is taken
+    User.findOne({username: username}, (err, user) => {
+      if (user) {
+        res.json({success: false, warning: 'That username is taken'});
+      } else {
+        // validation complete
+        username = username.trim();
+        password = password.trim();
+        User({username: username, password, password})
+          .save()
+          .then(() => {
+            console.log(`Created user: ${username}`);
+            res.json({success: true, uid: username});
+          });
+      }
+    });
+  }
 })
 
 router.get('/login', (req, res) => {
-  res.send('logging user in');
+  // get form inputs
+  let username = req.body['username'];
+  let password = req.body['password'];
+
+  // form validation
+  if (!username || username.trim() === '') {
+    res.json({success: false, warning: 'Username is required'});
+  } else if (!password || password.trim() === '') {
+    res.json({success: false, warning: 'Password is required'});
+  } else {
+    // check login info
+    User.findOne({username: username}, (err, user) => {
+      if (!user || user.password !== password) {
+        res.json({success: false, warning: 'Invalid login'});
+      } else {
+        // validation complete
+        res.cookie('user_id', username);
+        res.json({success: true, uid: username});
+      }
+    });
+  }
 })
 
 router.post('/new', (req, res) => {
